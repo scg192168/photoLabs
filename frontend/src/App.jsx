7f143f9dbd76f26ae0e9ca6c568fdbf6cd78eff8
Shyamal <scghoshbc@gmail.com>
2023-10-25 16:54:01 -0700

    complete display 3 photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+import React from "react";
+import PhotoListItem from "./components/PhotoListItem";
+import "./App.scss";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const sampleDataForPhotoListItem = [
+    {
+      id: "1",
+      location: {
+        city: "Montreal",
+        country: "Canada",
+      },
+      liked: false,
+      imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+      username: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+    {
+      id: "2",
+      location: {
+        city: "Montreal",
+        country: "Canada",
+      },
+      liked: true,
+      imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+      username: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+    {
+      id: "3",
+      location: {
+        city: "Montreal",
+        country: "Canada",
+      },
+      liked: false,
+      imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+      username: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+  ];
   return (
-    <div className="App">
-      <PhotoListItem samplePhoto= { sampleDataForPhotoListItem }/>
+    <div>
+      <h1>PhotoLabs</h1>
+      <div className="app">
+        {sampleDataForPhotoListItem.map((photo, index) => (
+          <PhotoListItem key={index} samplePhoto={photo} />
+        ))}
+      </div>
     </div>
   );
 };
-
 export default App;
