835f4bd01774e6c72f96f7b4d3a85582495a288e
Shyamal <scghoshbc@gmail.com>
2023-10-25 15:52:59 -0700

    complete apply styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import "../styles/PhotoListItem.scss";
 
-
 const PhotoListItem = (props) => {
   const samplePhoto = props.samplePhoto;
   /* Insert React */
-  return(
-    <div>
-     <img src={samplePhoto.imageSource}/>
-     <img src={samplePhoto.profile}/>
-     <h2> {props.samplePhoto.username} </h2>
-     <h2> {props.samplePhoto.location.city}, {props.samplePhoto.location.country} </h2>
+  return (
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={samplePhoto.imageSource} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={samplePhoto.profile} />
+        <div className="photo-list__user-info">
+          {props.samplePhoto.username}
+          <div className="photo-list__user-location">
+            {props.samplePhoto.location.city}
+            {props.samplePhoto.location.country}
+          </div>
+        </div>
       </div>
-  )
+    </div>
+  );
 };
 
 export default PhotoListItem;
